efc3cdb47e703d08456db2cad10021cf385569c7
yanxiao <yanxiao@mogujie.com>
2016-04-23 18:31:33 +0800

    add test package

---
+++ common/src/main/java/com/mogujie/service/springdemo/common/json/jsonpath/JsonPath.java
@@ -0,0 +1,11 @@
+package com.mogujie.service.springdemo.common.json.jsonpath;
+
+/**
+ * Created by yanxiao on 16/4/19.
+ */
+public class JsonPath {
+
+    public void parseJsonPath(){
+
+    }
+}
---
+++ common/src/test/java/com/mogujie/service/springdemo/common/json/jsonpath/JsonPathTest.java
@@ -0,0 +1,17 @@
+package com.mogujie.service.springdemo.common.json.jsonpath;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yanxiao on 16/4/19.
+ */
+public class JsonPathTest {
+
+    @Test
+    public void testParseJsonPath() throws Exception {
+        String json = "";
+
+    }
+}
