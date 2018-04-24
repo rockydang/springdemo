eddbf9a2dbdf2d24162368b697cfb698f7b44444
rockydang <rocky_dang@163.com>
2018-04-24 09:43:25 +0800

    add collectors

--- common/src/main/java/com/rockydang/service/springdemo/common/jdk8/stream/StreamTest10.java
+++ common/src/main/java/com/rockydang/service/springdemo/common/jdk8/stream/StreamTest10.java
@@ -2,11 +2,9 @@ package com.rockydang.service.springdemo.common.jdk8.stream;
 
 import com.rockydang.service.springdemo.common.jdk8.stream.vo.Student;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.*;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * 分组功能
@@ -22,9 +20,9 @@ public class StreamTest10 {
         Student student3 = new Student("li", 10, 80);
         List<Student> studentList = Arrays.asList(student1, student2, student3);
 
-       //按名字分组
+        //按名字分组
         Map<String, List<Student>> nameStudentMap = studentList.stream()
-                .collect(Collectors.groupingBy(Student::getName));
+                .collect(groupingBy(Student::getName));
         System.out.println(nameStudentMap);
 /*
         //按分数分组
@@ -44,7 +42,53 @@ public class StreamTest10 {
 
         //按人名分区、分数求平均值
         Map<Boolean, List<Student>> partitioningByMap = studentList.stream()
-                .collect(Collectors.partitioningBy(student -> student.getScore() >= 90));
+                .collect(partitioningBy(student -> student.getScore() >= 90));
         System.out.println(partitioningByMap);
+
+        System.out.println("#########################################");
+
+        //获取分数最低的学生
+        studentList.stream().collect(minBy(Comparator.comparing(Student::getScore))).ifPresent(System.out::println);
+        //获取分数最高的学生
+        studentList.stream().collect(maxBy(Comparator.comparing(Student::getScore))).ifPresent(System.out::println);
+        //获取平均值
+        System.out.println(studentList.stream().collect(averagingInt(Student::getScore)));
+        //获取分数总和
+        System.out.println(studentList.stream().collect(summingInt(Student::getScore)));
+
+        //分析
+        IntSummaryStatistics intSummaryStatistics = studentList.stream().collect(summarizingInt(Student::getScore));
+        System.out.println(intSummaryStatistics);
+        System.out.println("##################");
+
+        System.out.println(studentList.stream().map(Student::getName).collect(joining()));
+        System.out.println(studentList.stream().map(Student::getName).collect(joining(",")));
+        System.out.println(studentList.stream().map(Student::getName).collect(joining(",", "<begin>", "<end>")));
+        System.out.println("##################");
+        Map<Integer, Map<String, List<Student>>> map =
+                studentList.stream().collect(groupingBy(Student::getScore, groupingBy(Student::getName)));
+        System.out.println(map);
+
+        System.out.println("###############");
+        Map<Boolean, List<Student>> booleanListMap =
+                studentList.stream().collect(partitioningBy(student -> student.getScore() > 80));
+        System.out.println(booleanListMap);
+        System.out.println("###############");
+
+        Map<Boolean, Map<Boolean, List<Student>>> map3 =
+                studentList.stream().collect(
+                        partitioningBy(studentItem -> studentItem.getScore() > 80, partitioningBy(studentItem -> studentItem.getScore() > 90)));
+        System.out.println("map3=" + map3);
+
+        Map<Boolean, Long> map4 =
+                studentList.stream().collect(partitioningBy(student -> student.getScore() > 80, counting()));
+        System.out.println(map4);
+        System.out.println("-------------------------");
+
+        Map<String, Student> map5 =
+                studentList.stream().collect(groupingBy(Student::getName, collectingAndThen(minBy(Comparator.comparingInt(Student::getScore)), Optional::get)));
+        System.out.println(map5);
     }
+
+
 }
